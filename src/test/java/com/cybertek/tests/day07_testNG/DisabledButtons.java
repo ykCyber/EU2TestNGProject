d672d115c945b80cad2ce7fc2ebc5b78ae7f4dca
YusufKenan <59359546+ykCyber@users.noreply.github.com>
2020-04-12 22:31:18 +0300

    Assert

---
+++ src/test/java/com/cybertek/tests/day07_testNG/DisabledButtons.java
@@ -0,0 +1,21 @@
+package com.cybertek.tests.day07_testNG;
+
+import com.cybertek.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class DisabledButtons {
+    @Test
+    public void radioTest1() {
+        WebDriver driver = WebDriverFactory.getDriver("opera");
+        driver.get("http://practice.cybertekschool.com/radio_buttons");
+        driver.manage().window().maximize();
+        WebElement greenButton = driver.findElement(By.id("green"));
+        System.out.println("greenButton.isEnabled() = " + greenButton.isEnabled());
+        Assert.assertFalse(greenButton.isEnabled(),"Green Button Disen");
+    }
+}
+
