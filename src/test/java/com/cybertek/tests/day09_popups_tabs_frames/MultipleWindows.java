8a14e180e80d837a898bfe4cd0342c54af577bf6
YusufKenan <59359546+ykCyber@users.noreply.github.com>
2020-04-19 21:43:41 +0300

    DAY09

--- src/test/java/com/cybertek/tests/day09_popups_tabs_frames/MultipleWindows.java
+++ src/test/java/com/cybertek/tests/day09_popups_tabs_frames/MultipleWindows.java
@@ -7,22 +7,25 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import java.util.Iterator;
+import java.util.Set;
+
 public class MultipleWindows {
     WebDriver driver;
 
     @BeforeMethod
-    public void setUpMethod(){
+    public void setUpMethod() {
         driver = WebDriverFactory.getDriver("opera");
     }
 
     @AfterMethod
     public void afterMethod() throws InterruptedException {
         Thread.sleep(6000);
-       // driver.quit();
+        // driver.quit();
     }
 
     @Test
-    public void switchWindowsTest(){
+    public void switchWindowsTest() {
 
         driver.get("http://practice.cybertekschool.com/windows");
         System.out.println("driver.getTitle() = " + driver.getTitle());
@@ -30,6 +33,13 @@ public class MultipleWindows {
         driver.findElement(By.linkText("Click Here")).click();
         System.out.println("driver.getWindowHandle()After = " + driver.getWindowHandle());
         System.out.println(driver.getTitle());
+        driver.navigate().to("https://www.google.com.tr/");
+                Set<String> windowsIds = driver.getWindowHandles();
+        Iterator<String> iter = windowsIds.iterator();
+        String oldWindow = iter.next();
+        String newWindow = iter.next();
+        driver.switchTo().window(newWindow);
+        System.out.println("driverNEW PAGE = " + driver.getTitle());
     }
 
 }
