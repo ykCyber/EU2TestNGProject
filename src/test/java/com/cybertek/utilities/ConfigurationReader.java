4a00f0fa1d0f0c3d6894e25a43be902bc57254cc
ykCyber <Malatya44+>
2020-05-12 17:03:02 +0300

    day13

--- src/test/java/com/cybertek/utilities/ConfigurationReader.java
+++ src/test/java/com/cybertek/utilities/ConfigurationReader.java
@@ -2,13 +2,30 @@ package com.cybertek.utilities;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.util.Properties;
 
 public class ConfigurationReader {
+
+    private static Properties properties;
+
+    static {
+        try {
+            String path = "configuration.propoerties";
+
+            FileInputStream inputStream = new FileInputStream(path);
+            properties = new Properties();
+            properties.load(inputStream);
+            inputStream.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
     public static String get(String value) throws FileNotFoundException {
 
         String result = null;
 
-        FileInputStream inputStream = new FileInputStream("//idea/navigate/reference?project=EU2TestNGProject&path=configuration.properties" );
+        FileInputStream inputStream = new FileInputStream("//idea/navigate/reference?project=EU2TestNGProject&path=configuration.properties");
         System.out.println("inputStream.toString() = " + inputStream.toString());
 
         return inputStream.toString();
