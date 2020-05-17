b7c57c8aa5c0fdee1b44374a941681caa567bfa6
ykCyber <yusufkenan2604@gmail.com>
2020-05-17 18:28:59 +0300

    TestBaseUpdate

--- src/test/java/com/cybertek/tests/TestBase.java
+++ src/test/java/com/cybertek/tests/TestBase.java
@@ -1,33 +1,36 @@
 package com.cybertek.tests;
 
+import com.cybertek.utilities.ConfigurationReader;
 import com.cybertek.utilities.Driver;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Test;
 
 import java.util.concurrent.TimeUnit;
 
 public class TestBase {
+
     protected WebDriver driver;
     protected Actions actions;
     protected WebDriverWait wait;
 
     @BeforeMethod
-    public void setUp() {
+    public void setUpMethod(){
         driver = Driver.get();
-        actions = new Actions(driver);
-        wait = new WebDriverWait(driver, 10);
-        driver.manage().timeouts().implicitlyWait(8, TimeUnit.SECONDS);
         driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        actions = new Actions(driver);
+        wait = new WebDriverWait(driver,10);
+        driver.get(ConfigurationReader.get("url"));
+
     }
 
     @AfterMethod
     public void afterMethod() throws InterruptedException {
         Thread.sleep(2000);
-        driver.quit();
+        Driver.closeDriver();
         driver = null;
     }
 
