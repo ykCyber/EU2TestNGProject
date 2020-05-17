b00ad6cea374186efbd1d0b4b2964c5259e53d82
ykCyber <yusufkenan2604@gmail.com>
2020-05-17 23:09:32 +0300

    TestBaseUpdate

--- src/test/java/com/cybertek/utilities/BrowserUtils.java
+++ src/test/java/com/cybertek/utilities/BrowserUtils.java
@@ -29,6 +29,7 @@ public class BrowserUtils {
         // TakesScreenshot ---> interface from selenium which takes screenshots
         TakesScreenshot ts = (TakesScreenshot) Driver.get();
         File source = ts.getScreenshotAs(OutputType.FILE);
+        
         // full path to the screenshot location
         String target = System.getProperty("user.dir") + "/test-output/Screenshots/" + name + date + ".png";
         File finalDestination = new File(target);
